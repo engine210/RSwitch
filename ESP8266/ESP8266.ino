f6d5f92e97d8250b603cc9f48d71aabd0045d7a0
Steven Wang <engine210@gmail.com>
2020-03-21 10:38:20 +0800

    Change esp8266 to minimize function, removing webpage, and add /sw to turn on/off two switch togethor

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,19 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include "index.h"
 
 #define OUT_PIN1 2
 #define OUT_PIN2 0
 
-const char ssid[] = "engine_dorm_2.4GHz";
-const char pass[] = "0220!@#$";
-
-ESP8266WebServer server(80); // Run a web server on port 80
-
-// handaling the request of main page
-void rootRouter() {
-  server.send (200, "text/html", PAGE_INDEX );
-}
+ESP8266WebServer server(80);
 
 void setup() {
   pinMode(OUT_PIN1, OUTPUT);
@@ -21,7 +12,7 @@ void setup() {
   Serial.begin(115200);
 
   // Connecting to wifi
-  WiFi.begin(ssid, pass);
+  WiFi.begin("engine_dorm_2.4GHz", "0220!@#$");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -29,42 +20,42 @@ void setup() {
   Serial.println("");
   Serial.print("WiFi connected, IP: ");
   Serial.println(WiFi.localIP());  // print the ip
-
-  // the main page
-  server.on ( "/", rootRouter);
-  // page for switch 1
-  server.on ("/sw1", []() {
-     String state = server.arg("led");
-     if (state == "ON") {
-         digitalWrite(OUT_PIN1, HIGH);
-     } else if (state == "OFF") {
-         digitalWrite(OUT_PIN1, LOW);
-     }
-     
-     Serial.print("OUT_PIN1: ");
-     Serial.println(state);
+  
+  server.on ("/sw", HTTP_POST, []() {
+    if (server.arg("led") == "ON") {
+      digitalWrite(OUT_PIN1, HIGH);
+      digitalWrite(OUT_PIN2, HIGH);
+    } else {
+      digitalWrite(OUT_PIN1, LOW);
+      digitalWrite(OUT_PIN2, LOW);
+    }
+    server.send(200, "text/plain", "");
   });
-  // page for switch 2
-  server.on ("/sw2", []() {
-     String state = server.arg("led");
-     if (state == "ON") {
-         digitalWrite(OUT_PIN2, HIGH);
-     } else if (state == "OFF") {
-         digitalWrite(OUT_PIN2, LOW);
-     }
-     
-     Serial.print("OUT_PIN2: ");
-     Serial.println(state);
+  server.on ("/sw1", HTTP_POST, []() {
+    if (server.arg("led") == "ON") {
+      digitalWrite(OUT_PIN1, HIGH);
+    } else {
+      digitalWrite(OUT_PIN1, LOW);
+    }
+    server.send(200, "text/plain", "");
   });
+  server.on ("/sw2", HTTP_POST, []() {
+    if (server.arg("led") == "ON") {
+      digitalWrite(OUT_PIN2, HIGH);
+    } else {
+      digitalWrite(OUT_PIN2, LOW);
+    }
+    server.send(200, "text/plain", "");
+  });
+  
   // For page not found
   server.onNotFound([](){
     server.send(404, "text/plain", "404 NOT found!");
   });
   
   server.begin();
-  Serial.println("HTTP server started.");
 }
 
 void loop() {
-    server.handleClient(); // handaling requests
+  server.handleClient();
 }
